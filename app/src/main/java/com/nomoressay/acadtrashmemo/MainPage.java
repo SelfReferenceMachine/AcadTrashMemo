f4849d5a93a1d2d5fd26031ff62815c6a6cc626a
SelfReferenceMachine <953956895@qq.com>
2019-06-12 15:31:10 +0800

    i do not wanna die

--- app/src/main/java/com/nomoressay/acadtrashmemo/MainPage.java
+++ app/src/main/java/com/nomoressay/acadtrashmemo/MainPage.java
@@ -26,6 +26,9 @@ public class MainPage extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.action_settings);
+                    return true;
             }
             return false;
         }
---
+++ app/src/main/java/com/nomoressay/acadtrashmemo/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.nomoressay.acadtrashmemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.activity_splash);
+
+
+            Thread myThread=new Thread(){
+                @Override
+                public void run() {
+                    try{
+                        sleep(5000);
+                        Intent it=new Intent(getApplicationContext(),Login.class);
+                        startActivity(it);
+                        finish();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            };
+            myThread.start();
+
+        }
+    }
